eafeb4b2f3cfa7651850cc39f9c1c2897ee3aae4
yuvraj3225 <yuvrajlungade3774@gmail.com>
2024-02-23 22:45:54 +0530

    first commit

---
+++ src/store/index.js
@@ -0,0 +1,35 @@
+import {configureStore,createAsyncThunk,createSlice} from "@reduxjs/toolkit"
+import axios from 'axios'
+import { API_KEY, TMDB_BASE_URL } from "../utils/constant";
+
+const initialState = {
+  movies: [],
+  genresLoaded: false,
+  genres: []
+};
+
+export const getGenres = createAsyncThunk("netflix/genres", async() =>{
+  const {data} = await axios.get (
+    `${TMDB_BASE_URL}/genre/movie/list?api_key=${API_KEY}`
+  ); 
+  console.log(data);
+
+});
+
+const NetflixSlice = createSlice({
+  name : "Netflix",
+  initialState,
+  extraReducers: (builder) =>{
+
+    builder.addCase(getGenres.fulfilled,(state,action) =>{
+      state.genres = action.payload;
+      state.genresLoaded = true ;
+    })
+  },
+});
+
+export const store = configureStore({
+  reducer:{
+    netflix: NetflixSlice.reducer,
+  },
+});
